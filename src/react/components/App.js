f817729be315137400a8d01007e14e21afe83cb9
lucas carranza <lucas.carranza98@gmail>
2018-09-05 09:27:38 -0300

    'subiendo'

---
+++ src/react/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <p>Hola mundo</p>
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default App;
